02fb3471564a6603cf38e0b7025342f94aa0b6f6
samao <qiyanlong@wozine.com>
2018-08-29 16:11:25 +0800

    ts

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,10 @@
-declare module 'ajax-fetch' {
+declare module 'xhr-fetch' {
 
 	interface NodeLikeCallback {
 		(error: string, data?: string): void;
 	}
 
 	const ajaxFetch: (url: string, cb: NodeLikeCallback) => void;
-	
+
 	export default ajaxFetch;
 }
