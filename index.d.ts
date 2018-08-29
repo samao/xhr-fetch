7ce0232f1c5180f617755cf6c58d65eb266f7560
samao <qiyanlong@wozine.com>
2018-08-29 18:45:24 +0800

    readme

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,15 @@
+/*
+ * @Author: iDzeir 
+ * @Date: 2018-08-29 18:42:51 
+ * @Last Modified by:   iDzeir 
+ * @Last Modified time: 2018-08-29 18:42:51 
+ */
 declare module 'xhr-fetch' {
+    interface NodeLikeCallBack {
+        (error: string | null, data?: string): void;
+    }
 
-	interface NodeLikeCallBack {
-		(error: string | null, data?: string): void;
-	}
+    const xhrFetch: (url: string, cb: NodeLikeCallBack) => void;
 
-	const xhrFetch: (url: string, cb: NodeLikeCallBack) => void;
-
-	export default xhrFetch;
+    export default xhrFetch;
 }
--- index.js
+++ index.js
@@ -1,39 +1,48 @@
+/*
+ * @Author: iDzeir 
+ * @Date: 2018-08-29 18:42:45 
+ * @Last Modified by:   iDzeir 
+ * @Last Modified time: 2018-08-29 18:42:45 
+ */
 export default function xhrFetch(url, cb) {
-	if(window.fetch) {
-		fetch(url).then(function(res){
-			return res.text();
-		}).then(function(data){
-			cb(null, data);
-		}).catch(function(reason) {
-			cb(reason)
-		})
-	}else{
-		var xhr;
-		if(window.XMLHttpRequest) {
-			xhr = new XMLHttpRequest();
-		}else{
-			xhr = new ActiveXObject('Microsoft.XMLHTTP');
-		}
-		xhr.open('GET', url);
-		xhr.onload = function() {
-			clear();
-		}
-		xhr.onerror = function() {
-			clear();
-		}
-		xhr.onreadystatechange = function() {
-			if(xhr.readtState === 4) {
-				if(xhr.status >= 400) {
-					cb('Error:' + xhr.status);
-				}else {
-					cb(null, xhr.responseText);
-				}
-				clear();
-			}
-		}
+    if (window.fetch) {
+        fetch(url)
+            .then(function(res) {
+                return res.text();
+            })
+            .then(function(data) {
+                cb(null, data);
+            })
+            .catch(function(reason) {
+                cb(reason);
+            });
+    } else {
+        var xhr;
+        if (window.XMLHttpRequest) {
+            xhr = new XMLHttpRequest();
+        } else {
+            xhr = new ActiveXObject('Microsoft.XMLHTTP');
+        }
+        xhr.open('GET', url);
+        xhr.onload = function() {
+            clear();
+        };
+        xhr.onerror = function() {
+            clear();
+        };
+        xhr.onreadystatechange = function() {
+            if (xhr.readtState === 4) {
+                if (xhr.status >= 400) {
+                    cb('Error:' + xhr.status);
+                } else {
+                    cb(null, xhr.responseText);
+                }
+                clear();
+            }
+        };
 
-		function clear(){
-			xhr.onload = xhr.onerror = xhr.onreadystatechange = null;
-		}
-	}
+        function clear() {
+            xhr.onload = xhr.onerror = xhr.onreadystatechange = null;
+        }
+    }
 }
